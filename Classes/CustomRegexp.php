b4e95f84905bd253ae8b1399304e74a29a8ec4ef
Yurii Miroshnyk <lemadioro@gmail.com>
2024-06-19 16:01:24 +0300

    TASK: regexp: add one more variant of check source string

--- Classes/CustomRegexp.php
+++ Classes/CustomRegexp.php
@@ -3,8 +3,11 @@
 namespace Classes;
 class CustomRegexp
 {
-    private const string REGEXP_BY_DOMAIN = '/^[0-9a-zA-Z]+([.-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+\.(ua|com|net)$/i';
+    private const string REGEXP_BY_DOMAIN = '/^[0-9a-zA-Z]+([\.-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+\.(ua|com|net)$/i';
     private const string REGEXP_BY_DOMAIN_CHARS_COUNT = '/^[0-9a-zA-Z]+([\.-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+\.[0-9a-zA-Z]{2,3}$/i';
+
+    private const string REGEXP_BY_DOMAIN_COMPACT = '/^\w+([\.-]\w+)*@\w+\.[0-9a-zA-Z]{2,3}$/i';
+
 
     public static function testMatchByAllowedDomain(string $testString): bool
     {
@@ -13,9 +16,12 @@ class CustomRegexp
 
     public static function testMatchByDomainCharCount(string $testString): bool
     {
-
         return preg_match(self::REGEXP_BY_DOMAIN_CHARS_COUNT, $testString);
+    }
 
+    public static function testMatchByDomainCompact(string $testString): bool
+    {
+        return preg_match(self::REGEXP_BY_DOMAIN_COMPACT, $testString);
     }
 }
 
